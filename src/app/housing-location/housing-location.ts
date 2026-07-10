fd0ae932ee9e5d624e4a4710d0c7878301785cb1
Eric Babcock <eric.babcock@protonmail.com>
2026-07-10 16:57:49 +0200

    finished lesson 3

---
+++ src/app/housing-location/housing-location.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: `
+    housing-location works!
+  `,
+  styleUrls: [`housing-location.css`],
+})
+export class HousingLocation {
+
+}

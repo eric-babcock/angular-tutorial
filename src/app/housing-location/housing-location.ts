96c0d02fba58f70c4c2e4f638c8a620fec47c85d
Eric Babcock <eric.babcock@protonmail.com>
2026-07-13 15:41:22 +0200

    finished lesson 5

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, input } from '@angular/core';
+import {HousingLocationInfo} from "../housinglocation";
 
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  //imports: [],
   template: `
-    housing-location works!
+    <p>housing-location works!</p>
   `,
   styleUrls: [`housing-location.css`],
 })
 export class HousingLocation {
-
+  housingLocation = input.required<HousingLocationInfo>();
 }
